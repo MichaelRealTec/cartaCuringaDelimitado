190e915ee83e904ebca990bbffdfdf50c1231858
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2023-12-26 13:26:55 -0300

    Problema 1

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,11 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Problema 1
+		/* Vamos fazer um método para retornar a soma das áreas de uma lista de figuras.
+		 */
+		
+		
 
 	}
 
