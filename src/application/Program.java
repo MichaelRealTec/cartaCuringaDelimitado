b61cb582b78708e244e900d7c5f8958d09add972
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2023-12-26 16:33:37 -0300

    problema 2 (método que copia uma lista para outra lista)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,44 +1,57 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import entities.Circle;
-import entities.Rectangle;
-import entities.Shape;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// Problema 1
+		// Problema 2
 		/*
-		 * Vamos fazer um método para retornar a soma das áreas de uma lista de figuras.
+		 * Vamos fazer um método que copia os elementos de uma lista para uma outra
+		 * lista que pode ser mais genérica que a primeira.
 		 */
 
-		/*
-		 * Nota 1: soluções impróprias: public double totalArea(List<Shape> list) public
-		 * double totalArea(List<?> list)
-		 * 
-		 * Nota 2: não conseguiremos adicionar elementos na lista do método
-		 */
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDouble = Arrays.asList(3.14, 6.28);
+		List<Object> myObjs = new ArrayList<Object>();
 
-		List<Shape> myShapes = new ArrayList<>();
-		myShapes.add(new Rectangle(3.0, 2.0));
-		myShapes.add(new Circle(2.0));
+		copy(myInts, myObjs);
+		copy(myDoubles, myObjs);
 
-		List<Circle> myCircles = new ArrayList<>();
-		myCircles.add(new Circle(2.0));
-		myCircles.add(new Circle(3.0)); 
-				
-		System.out.println("Total area: " + totalArea(myShapes));
-	}
-
-	public static double totalArea(List<? extends Shape> list) {
-		double sum = 0.0;
-		for (Shape s : list) {
-			sum += s.area();
-		}
-		return sum;
 	}
 
 }
+
+/*
+ * Comentários
+ * 
+ * // Problema 1 /* Vamos fazer um método para retornar a soma das áreas de uma
+ * lista de figuras.
+ */
+
+/*
+ * Nota 1: soluções impróprias: public double totalArea(List<Shape> list) public
+ * double totalArea(List<?> list)
+ * 
+ * Nota 2: não conseguiremos adicionar elementos na lista do método
+ */
+
+/*
+ * List<Shape> myShapes = new ArrayList<>(); myShapes.add(new Rectangle(3.0,
+ * 2.0)); myShapes.add(new Circle(2.0));
+ * 
+ * List<Circle> myCircles = new ArrayList<>(); myCircles.add(new Circle(2.0));
+ * myCircles.add(new Circle(3.0));
+ * 
+ * System.out.println("Total area: " + totalArea(myShapes)); }
+ * 
+ * public static double totalArea(List<? extends Shape> list) { double sum =
+ * 0.0; for (Shape s : list) { sum += s.area(); } return sum;
+ * 
+ * 
+ * }
+ * 
+ * }
+ */
